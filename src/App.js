280ca572c52b95af5d732b14c1340c82c3b6e307
Willochs316 <willochs316@gmail.com>
2023-04-23 02:07:34 +0100

    modified useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import Giphy from "./components/Giphy/Giphy";
 import NavBar from "./components/NavBar/NavBar";
-import axios from "axios";
-import "./App.css";
 import Spinner from "./components/Spinner/Spinner";
+import "./App.css";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = "https://api.giphy.com/v1/gifs/";
@@ -14,19 +14,30 @@ const App = () => {
   const [isError, setIsError] = useState(false);
   const [searchValue, setSearchValue] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [shouldFetchTrending, setShouldFetchTrending] = useState(true);
 
   useEffect(() => {
-    const fetchGifs = async () => {
+    const fetchData = async () => {
       setIsError(false);
       setIsLoading(true);
 
       try {
         let endpoint = "";
 
-        endpoint = `${API_URL}trending?api_key=${API_KEY}&limit=25&offset=0&rating=Y&lang=en`;
+        if (shouldFetchTrending) {
+          endpoint = `${API_URL}trending?api_key=${API_KEY}&limit=25&offset=0&rating=Y&lang=en`;
+        } else {
+          endpoint = `${API_URL}search?q=${searchValue}&api_key=${API_KEY}&limit=25&offset=${
+            (currentPage - 1) * 25
+          }&rating=Y&lang=en`;
+        }
 
         const results = await axios.get(endpoint);
-        setData(results.data.data);
+        setData((prevData) =>
+          shouldFetchTrending
+            ? [...prevData, ...results.data.data]
+            : results.data.data
+        );
       } catch (error) {
         setIsError(true);
         console.error(error);
@@ -35,8 +46,38 @@ const App = () => {
       setIsLoading(false);
     };
 
-    fetchGifs();
-  }, [searchValue]);
+    if (shouldFetchTrending) {
+      fetchData();
+    }
+  }, [shouldFetchTrending]);
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+    setShouldFetchTrending(false);
+
+    try {
+      const endpoint = `${API_URL}search?api_key=${API_KEY}&q=${searchValue}&_limit=25&rating=Y&lang=en`;
+      const results = await axios.get(endpoint);
+
+      setData(results.data.data);
+      setCurrentPage(1);
+    } catch (error) {
+      setIsError(true);
+      console.error(error);
+    }
+
+    setIsLoading(false);
+    setSearchValue("");
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    handleSearch();
+  };
+
+  const handleClick = async () => {
+    handleSearch();
+  };
 
   const loadMoreData = async () => {
     try {
@@ -66,28 +107,12 @@ const App = () => {
     }
   };
 
-  const handleClick = async (event) => {
-    event.preventDefault();
-    setIsLoading(true);
-
-    try {
-      const endpoint = `${API_URL}search?api_key=${API_KEY}&q=${searchValue}&_limit=25&rating=Y&lang=en`;
-      const results = await axios.get(endpoint);
-
-      setData(results.data.data);
-    } catch (error) {
-      setIsError(true);
-      console.error(error);
-    }
-
-    setIsLoading(false);
-  };
-
   return (
     <div className="giphy-container">
       <NavBar
         searchValue={searchValue}
         setSearchValue={setSearchValue}
+        handleSubmit={handleSubmit}
         handleClick={handleClick}
       />
 
